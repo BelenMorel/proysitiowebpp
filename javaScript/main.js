be63fb2ea2b5a90792dedf48da1f526fe0c41e0a
BelenMorel <Belenmmorel@gmail.com>
2023-10-06 22:43:07 -0300

    seccion bienvenida

--- javaScript/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var myCarousel = new bootstrap.Carousel(document.querySelector('#carouselExampleSlidesOnly'), {
-        interval: 10000, // Cambia el intervalo a la cantidad de tiempo que desees entre las imágenes (en milisegundos)
-        wrap: true // Establece esto en false si no deseas que el carrusel se "envuelva" después de llegar a la última imagen
-    });
-});
