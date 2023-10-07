52d77894187665785e9a14a8e328b893726dbf6e
BelenMorel <Belenmmorel@gmail.com>
2023-10-07 03:04:04 -0300

    nuevo encabezado

---
+++ javaScript/main.js
@@ -0,0 +1,4 @@
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+    // Muestra el contenido del menú cuando se hace clic en el botón
+    document.querySelector('.collapse').classList.toggle('show');
+});
