41ba044bd9336cdff55db0b276632fcced35e7d2
BelenMorel <belenmmorel@gmail.com>
2023-10-14 03:38:39 -0300

    paginaContactos

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,3 +2,40 @@
 document.querySelector('.navbar-toggler').addEventListener('click', function() {
  document.querySelector('.collapse').classList.toggle('show');
 });
+
+// Boton para copiar nro
+
+document.getElementById("boton-copiar").addEventListener("click", function() {
+    var textoACopiar = document.getElementById("nrotel").textContent;
+    
+    var campoTemporal = document.createElement("textarea");
+    campoTemporal.value = textoACopiar;
+  
+    document.body.appendChild(campoTemporal);
+  
+    campoTemporal.select();
+    campoTemporal.setSelectionRange(0, 99999);
+  
+    document.execCommand("copy");
+  
+    document.body.removeChild(campoTemporal);
+  
+  });
+  
+  // Boton para copiar nro
+  document.getElementById("boton-copiar").addEventListener("click", function() {
+    var textoACopiar = document.getElementById("dircorreo").textContent;
+    
+    var campoTemporal = document.createElement("textarea");
+    campoTemporal.value = textoACopiar;
+  
+    document.body.appendChild(campoTemporal);
+  
+    campoTemporal.select();
+    campoTemporal.setSelectionRange(0, 99999);
+  
+    document.execCommand("copy");
+  
+    document.body.removeChild(campoTemporal);
+  
+  });
